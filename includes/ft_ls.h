4023281b3c57acfde5bac385381fc7702516b72f
Sabri MAKNI <smakni@e1r7p13.42.fr>
2019-07-10 19:00:29 +0200

    add options

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,27 +6,24 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 15:54:56 by smakni            #+#    #+#             */
-/*   Updated: 2019/07/08 18:36:49 by smakni           ###   ########.fr       */
+/*   Updated: 2019/07/10 17:45:00 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
-#include <libft.h>
-#include <ft_printf.h>
-#include <get_next_line.h>
-#include <sys/dir.h>
-#include <sys/stat.h>
+# include <libft.h>
+# include <ft_printf.h>
+# include <get_next_line.h>
+# include <sys/dir.h>
+# include <sys/stat.h>
 
-#define		ERROR 		-1
-#define		CAPACITY	1024
+#define MAX_PATH 4096
 
-typedef	struct	s_env
-{
-	DIR				*dir[CAPACITY];
-	int				nb_dir;
-//	struct dirent 	*dirp[CAPACITY];
-}				t_env;
+# define R 1
+# define L 2
+# define A 4
+# define ALL (FLAG1 | FLAG2 | FLAG3)
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,68 +3,120 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
+/*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 15:54:19 by smakni            #+#    #+#             */
-/*   Updated: 2019/07/10 02:17:12 by sabri            ###   ########.fr       */
+/*   Updated: 2019/07/10 18:59:02 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_ls.h>
 
-#define MAX_NOM 1024
-
-void	lst_dir(char *rep, void (*fcn)(char *))
+void	print_stat(char *name, char *opt, char *path)
 {
-	char			nom[MAX_NOM];
-	struct dirent	*pr;
-	DIR				*fdr;
-	int				opt = 1;
-
-	if ((fdr = opendir(rep)) == NULL)
+	struct stat tmp;
+	
+	if ((stat(path, &tmp)) == -1)
 	{
-		ft_printf("balayer_rep : impossibe d'ouvrir %s\n", rep);
+		ft_printf("print_stat : acces impossible a %s\n", name);
 		return ;
 	}
-	while ((pr = readdir(fdr)) != NULL)
+	if (*opt & L)
 	{
-		if (ft_strcmp(pr->d_name, "..") == 0
-			|| ft_strcmp(pr->d_name, ".") == 0)
-			continue;
-		if (ft_strlen(rep) + ft_strlen(pr->d_name) + 2 > sizeof(nom))
-			ft_printf("balayer_rep : nom %s %s trop long\n", rep, pr->d_name);
-		else if (opt == 1)
-		{
-			ft_printf("%s\n", pr->d_name);
-			ft_sprintf(nom, "%s/%s", rep, pr->d_name);
-		 	(*fcn)(nom);
-		}
+		ft_printf("permissions ");
+		ft_printf("%d ", tmp.st_mode);
+		ft_printf("Ownder_name ");
+		ft_printf("group_name ");
+		ft_printf("%6u ", tmp.st_size);
+		//ft_printf("%s ", asctime(gmtime(&f_stat->st_mtimespec)));
 	}
-	closedir(fdr);
+	ft_printf("%s\n", name);
 }
 
-void	get_info(char *nom)
+void	lst_dir(char *f_name, void (*get_info)(char *, char *), char *opt)
 {
-	struct	stat	sttamp;
+	char			path[MAX_PATH];
+	struct dirent	*dir_ent;
+	DIR				*dir;
 
-	if ((stat(nom, &sttamp)) == -1)
+	if ((dir = opendir(f_name)) == NULL)
 	{
-		ft_printf("tailled : acces impossible a %s\n", nom);
+		ft_printf("balayer_rep : impossibe d'ouvrir %s\n", f_name);
 		return ;
 	}
-	if (__S_ISTYPE(sttamp.st_mode, __S_IFDIR))
+	while ((dir_ent = readdir(dir)) != NULL)
 	{
-		ft_printf(">>>%s:\n", nom);
-		lst_dir(nom, get_info);
+		if (dir_ent->d_name[0] == '.')
+		{
+			if (*opt & A)
+			{
+				print_stat(dir_ent->d_name, opt, path);
+				if ((*opt & R)
+				&& ft_strcmp(dir_ent->d_name, ".") != 0
+				&& ft_strcmp(dir_ent->d_name, "..") != 0)
+				{
+					ft_sprintf(path, "%s/%s", f_name, dir_ent->d_name);
+		 			(*get_info)(path, opt);
+				}
+			}
+			continue ;
+		}
+		else if (*opt & R)
+		{
+			ft_sprintf(path, "%s/%s", f_name, dir_ent->d_name);
+		 	(*get_info)(path, opt);
+		}
+		print_stat(dir_ent->d_name, opt, path);
 	}
+	closedir(dir);
+}
+
+void	get_info(char *f_name, char *opt)
+{
+	struct	stat	f_stat;
+
+	if ((stat(f_name, &f_stat)) == -1)
+	{
+		ft_printf("get_info : acces impossible a %s\n", f_name);
+		return ;
+	}
+	if ((f_stat.st_mode & S_IFMT) == S_IFDIR)
+		lst_dir(f_name, get_info, opt);
+}
+
+void	fill_opt(char *av, char *opt)
+{
+	if (ft_strchr(av, 'R') != 0)
+		*opt |= 1;
+	if (ft_strchr(av, 'l') != 0)
+		*opt |= 2;
+	if (ft_strchr(av, 'a') != 0)
+		*opt |= 4;
 }
 
 int		main(int ac, char **av)
 {
+	char opt;
+	int i;
+
+	opt = 0;
+	i = 1;
 	if (ac == 1)
-		get_info(".");
+	 	get_info(".", &opt);
 	else
-		while(--ac > 0)
-			get_info(*++av);
+	{
+		while(i < ac)
+		{
+			if (av[i][0] == '-')
+			{
+				fill_opt(av[i], &opt);
+				if (i + 1 == ac)
+					get_info(".", &opt);
+			}
+			else
+				get_info(av[i], &opt);
+			i++;
+		}
+	}
     return (0);
 }
